4cff699eae1e323297c9414a293618aa1fae37fd
NS Devaraj <nsdevaraj@gmail.com>
2022-09-30 22:17:24 +0530

    step 5

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express")
 const app = express()
 
 app.get('/', (req,res) => {
-    res.send("<h1>Header here</h1>")
+    res.send("<h1>Header heress</h1>")
 })
-const port= process.env.port || 4000
+console.log( process.env.port, 'port env')
+const port= process.env.port || 4001
 app.listen(port, ()=> console.log(`${port} running`))
