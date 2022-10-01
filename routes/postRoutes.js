f3d175b09de2b6bfccbf23b4f1afd635b9fa2cdf
NS Devaraj <nsdevaraj@gmail.com>
2022-10-01 21:57:35 +0530

    fix express delete

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,7 +3,5 @@ const postController = require("../controllers/postController")
 const router = express.Router()
 //localhost:3000
 router.route('/').get(postController.getAllPosts).post(postController.createPost)
-router.route('/:id').get(postController.getOnePost).patch(postController.updatePost).delete(function(req, res){
-    postController.delete
-  })
+router.route('/:id').get(postController.getOnePost).patch(postController.updatePost).delete(postController.deletePost)
 module.exports = router;
