9ccc8b5931533c1e26ded781f39aec309810b3fa
NS Devaraj <nsdevaraj@gmail.com>
2022-10-02 05:10:09 +0530

    login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,45 @@
 const User = require('../models/userModel')
 const Post = require('../models/userModel')
-const bcrypt = require('bcrypt') 
+const bcrypt = require('bcrypt')
 
 exports.signUp = async (req, res) => {
-    const {username, password} = req.body
-    const hashpwd = await bcrypt.hashpwd(password,12)
+    const { username, password } = req.body
     try {
 
-        const newUser = await User.create( {username, password:hashpwd})
-        res.status(200).json({ 
-            status: 'success', 
-            data:{
+        const hashpwd = await bcrypt.hash(password, 12)
+        const newUser = await User.create({ username, password: hashpwd })
+        res.status(200).json({
+            status: 'success',
+            data: {
                 user: newUser,
             },
         })
     } catch (error) {
         res.status(400).json({ status: 'fail' })
+    }
+
+}
+
+exports.login = async (req, res) => {
+    const { username, password } = req.body
+    
+    try {
+        const hashpwd = await bcrypt.hash(password, 12)
+        const user = await User.findOne({username})
+        if (!user) res.status(404).json({ status: 'no user fail' })
+        const isCorrect = await bcrypt.compare(password, user.password)
+         if (isCorrect) {
+            res.status(200).json({
+                status: 'success login'+hashpwd+' '+user.password,
+            })
+        }else{
+
+            res.status(200).json({
+                status: 'failed pwd login',
+            })
+        }
+    } catch (error) {
+        res.status(400).json({ status: 'fail login' })
     }
 
 }
