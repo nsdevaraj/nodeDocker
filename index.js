2e7caf607f5b9ea1a3fd622d423a86b1d2274165
NS Devaraj <nsdevaraj@gmail.com>
2022-10-01 14:03:59 +0530

    reorder

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose")
 const {MONGO_IP, MONGO_PORT, MONGO_USER,MONGO_PWD} = require("./config/config")
  
 const app = express()
-mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PWD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`).then(()=>console.log('connected now')).catch((e)=>console.log(e))
+const mongourl=`mongodb://${MONGO_USER}:${MONGO_PWD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+mongoose.connect(mongourl).then(()=>console.log('connected now!')).catch((e)=>console.log(e))
 app.get('/', (req,res) => {
-    res.send("<h1>mongoooHeader here!</h1>")
+    res.send("<h1>mongooo Header here!</h1>")
 })
 console.log( process.env.PORT, 'port env')
 const port= process.env.PORT || 4001
