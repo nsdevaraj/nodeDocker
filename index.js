acc2795a1a69b96e06d984dc562e871303a1642b
NS Devaraj <nsdevaraj@gmail.com>
2022-10-01 21:38:58 +0530

    express set

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const { MONGO_IP, MONGO_PORT, MONGO_USER, MONGO_PWD } = require("./config/config")
+const postRouter = require('./routes/postRoutes')
 
 const app = express()
 const mongourl = `mongodb://${MONGO_USER}:${MONGO_PWD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
-
 const connectRetry = () => {
     mongoose
         .connect(mongourl)
@@ -21,5 +21,7 @@ app.get('/', (req, res) => {
     res.send("<h1>mongooo Headers here!!</h1>")
 })
 console.log(process.env.PORT, 'port env')
+//localhost:3000/posts
+app.use("/posts", postRouter)
 const port = process.env.PORT || 4001
 app.listen(port, () => console.log(`${port} running`))
