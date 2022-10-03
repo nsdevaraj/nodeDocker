daf69bdbc22df2062f9206073d71ff66c0e6c896
NS Devaraj <nsdevaraj@gmail.com>
2022-10-03 09:10:24 +0530

    cookie not working

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const session = require("express-session")
+let connectRedis = require('connect-redis');
 
 const cookieParser = require('cookie-parser');
 const redis = require("redis")
 
-let RedisStore = require("connect-redis")(session)
+let RedisStore = connectRedis(session); 
 const { MONGO_IP, MONGO_PORT, MONGO_USER, MONGO_PWD, REDIS_URL, REDIS_PORT, SESSION_SECRET } = require("./config/config")
 
 let redisClient = redis.createClient({
@@ -39,7 +40,7 @@ connectRetry()
 app.get('/', (req, res) => {
     res.send("<h1>mongooo Headers here!!</h1>")
 })
- 
+
 connectRedis = async () => {
 
     try {
@@ -50,24 +51,24 @@ connectRedis = async () => {
 
 }
 connectRedis()
+
 app.use(
-   session({
-     store: new RedisStore({ client: redisClient}),
-     resave:false,
-     name: 'innovate',
-     saveUninitialized:false,
-     secret:SESSION_SECRET,
-     cookie:{
-       httpOnly:true,
-       maxAge: 30000,
-       secure:false
-     },
-   })
- ); 
+    session({
+        store: new RedisStore({ client: redisClient }),
+        secret: SESSION_SECRET,
+        saveUninitialized: false,
+        resave: false, 
+        cookie: {
+            secure: false, // if true: only transmit cookie over https, in prod, always activate this
+            httpOnly: true, // if true: prevents client side JS from reading the cookie
+            maxAge: 1000 * 30, // session max age in milliseconds 30 secs 
+        },
+    })
+);
 
 app.use(express.json())
 
-//app.use(cookieParser());
+app.use(cookieParser());
 
 //localhost:3000/posts
 app.use("/posts", postRouter)
@@ -75,4 +76,4 @@ app.use("/users", userRouter)
 
 const port = process.env.PORT || 4001
 console.log(process.env.PORT, 'port env')
-app.listen(port, () => console.log(`${port} running`))
+app.listen(port, () => console.log(`${port} running now`))
