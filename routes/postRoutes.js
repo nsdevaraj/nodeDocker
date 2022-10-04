a72e91af6c9eba4b564e619ce42c8ee1db5ebfc0
NS Devaraj <nsdevaraj@gmail.com>
2022-10-04 11:45:14 +0530

    middleware

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const postController = require("../controllers/postController")
 const router = express.Router()
+const protect = require("../middleware/authMiddleware")
 //localhost:3000
-router.route('/').get(postController.getAllPosts).post(postController.createPost)
+router.route('/').get(postController.getAllPosts).post(protect, postController.createPost)
 router.route('/:id').get(postController.getOnePost).patch(postController.updatePost).delete(postController.deletePost)
 module.exports = router;
